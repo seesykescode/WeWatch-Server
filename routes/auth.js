300bdb244a7b9bd9350ac19c168e79099e1bd929
Johnathon Sykes <hireme@mrjsykes.com>
2017-12-16 12:48:36 -0800

    modified BASE_URL env variable

--- routes/auth.js
+++ routes/auth.js
@@ -4,12 +4,12 @@ const
     passport = require("passport"),
     twitchStrat = require("passport-twitch").Strategy
     
-
+console.log(`${process.env.BASE_URL}/auth/callback`)
 // Passport configuration
     passport.use(new twitchStrat({
         clientID: process.env.TWITCH_CLIENT_ID,
         clientSecret: process.env.TWITCH_CLIENT_SECRET,
-        callbackURL: `${process.env.BASE_URL}\auth\callback`,
+        callbackURL: `${process.env.BASE_URL}/auth/callback`,
         "scope": "user_read"
     },
         function (accessToken, refreshToken, profile, done) {
